d2f13bef73720834dbf488b092321633604c7750
kvn <x-optima@github.com>
2026-04-26 19:41:18 +0300

    git-rebase 1

--- rebase.sh
+++ rebase.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/bin/bash<span style="color: red;"> ## ОТВЕТ 13
+ 
+Скриншоты.  
+![Задание 13. Скриншот 1](screenshots/scr113_1.png)  
 # display command line options
 
 count=1
-for param in "$*"; do
-    echo "\$* Parameter #$count = $param"
+for param in "$@"; do
+    echo "Next parameter: $param"
     count=$(( $count + 1 ))
 done
+
+echo "====="
