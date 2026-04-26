a1b6386c1150c263d36aeca9583ed429b52c3a04
kvn <x-optima@github.com>
2026-04-26 20:22:16 +0300

    +1

--- rebase.sh
+++ rebase.sh
@@ -1,9 +1,5 @@
-#!/bin/bash<span style="color: red;"> ## ОТВЕТ 13
+#!/bin/bash
  
-Скриншоты.  
-![Задание 13. Скриншот 1](screenshots/scr113_1.png)  
-# display command line options
-
 count=1
 for param in "$@"; do
     echo "Next parameter: $param"
